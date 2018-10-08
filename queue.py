7141d225ff92890d66f34e7738303f50c8fc3dde
R_J <ronaldroserose@gmail.com>
2018-10-08 12:03:15 -0400

    queue

---
+++ queue.py
@@ -0,0 +1,36 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def insert(self, data):
+        self.queue.insert(0, data)
+
+    def peek(self):
+        return self.queue[-1]
+
+    def pop(self):
+        if len(self.queue):
+            self.queue.pop()
+        else:
+            print("Queue is empty")
+
+    def view(self):
+        i = len(self.queue) - 1
+        while i >= 0:
+            print(self.queue[i])
+            i -= 1
+
+
+obj = Queue()
+obj.insert("Mon")
+obj.insert("Tue")
+obj.insert("Wed")
+obj.insert("Thur")
+print("\nViewing all the elements")
+obj.view()
+print("\nRemoving the first element Mon")
+obj.pop()
+obj.view()
+print("\nPeek first element ", obj.peek())
+
+
